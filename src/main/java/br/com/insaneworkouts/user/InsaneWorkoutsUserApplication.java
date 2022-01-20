de28c8a4912f7880c01fe56408e16faf131dd39c
wtrindades <wtrindades@gmail.com>
2022-01-20 18:50:56 -0300

    build: init InsaneWorkouts - User Microservice project

---
+++ src/main/java/br/com/insaneworkouts/user/InsaneWorkoutsUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.insaneworkouts.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaneWorkoutsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsaneWorkoutsUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/insaneworkouts/user/InsaneWorkoutsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insaneworkouts.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsaneWorkoutsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
