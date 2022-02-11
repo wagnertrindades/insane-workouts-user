cb57442c8334ab0c5118aafaf46d335ff8c776bb
wtrindades <wtrindades@gmail.com>
2022-02-11 12:22:28 -0300

    chore:  add setters in User update form

--- src/main/java/br/com/insaneworkouts/user/controller/form/UserUpdateForm.java
+++ src/main/java/br/com/insaneworkouts/user/controller/form/UserUpdateForm.java
@@ -17,8 +17,16 @@ public class UserUpdateForm {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
