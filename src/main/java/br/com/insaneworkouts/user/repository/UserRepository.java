6f8b8090b537bf15e077b4806d6e41f6214c4de0
wtrindades <wtrindades@gmail.com>
2022-02-26 07:54:33 -0300

    feat: add find by email in UserRepository

--- src/main/java/br/com/insaneworkouts/user/repository/UserRepository.java
+++ src/main/java/br/com/insaneworkouts/user/repository/UserRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import br.com.insaneworkouts.user.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long>{
 
+    Optional<User> findByEmail(String email);
 }
