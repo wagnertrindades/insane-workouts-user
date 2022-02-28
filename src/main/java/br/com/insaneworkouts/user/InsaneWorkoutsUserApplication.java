71ae681d2022dd9203327f1bd80ca186b1c8fc37
wtrindades <wtrindades@gmail.com>
2022-02-28 08:37:36 -0300

    feat: add password encoder in application context

--- src/main/java/br/com/insaneworkouts/user/InsaneWorkoutsUserApplication.java
+++ src/main/java/br/com/insaneworkouts/user/InsaneWorkoutsUserApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class , ManagementWebSecurityAutoConfiguration.class})
 public class InsaneWorkoutsUserApplication {
@@ -12,4 +15,8 @@ public class InsaneWorkoutsUserApplication {
 		SpringApplication.run(InsaneWorkoutsUserApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
